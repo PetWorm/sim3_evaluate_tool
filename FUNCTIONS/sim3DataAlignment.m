2159f3a94e92d02dc906178451fae188b3513310
QIU Xiaochen <ares43490@126.com>
2019-07-08 16:13:40 +0800

    add sensors setup mod

--- FUNCTIONS/sim3DataAlignment.m
+++ FUNCTIONS/sim3DataAlignment.m
@@ -1,4 +1,4 @@
-%% align data by sim3 method
+%% align data by sm3 method
 %   Author:         Xiaochen Qiu from Beihang Univ.
 %   Reference:      "Least-Squares Estimation of Transformation Parameters 
 %                   between Two Point Patterns"
@@ -7,17 +7,24 @@
 %-Inputs:
 % @X: 3*n data to be aligned with @Y
 % @Y: 3*n data to be aligned with @X
+% @mod: sensors setup mode, deciding the alignment method
 %-Outputs:
 % @R: rotation
 % @t: transition
 % @s: scale
-function [R, t, s] = sim3DataAlignment (X, Y)
+function [R, t, s] = sim3DataAlignment (X, Y, mod)
 % Assertion
 if ( size(X,1)~=3 || size(Y,1)~=3 )
     error('TERMINATED BY FUNC Sm3DataAlignment: INPUT IS NOT 3D VECTOR');
 end
 if ( size(X,2)~=size(Y,2) )
     error('TERMINATED BY FUNC Sm3DataAlignment: INPUTS SIZE UNMATCHED');
+end
+if nargin==2
+    mod = 'mono';
+end
+if ~strcmp(mod,'mono') && ~strcmp(mod,'stereo') && ~strcmp(mod,'vio')
+    error('SENSORS SETUP IS NOT EXISTED, SHOULD BE ''mono'' (DEFAULT), ''stereo'' or ''vio''');
 end
 % calculate some necesssary quantity
 n = size(X,2);
@@ -30,19 +37,33 @@ for i = 1:n
     cov = (Y(:,i)-mu_Y) * (X(:,i)-mu_X)';
     covXY = covXY + cov;
 end
-covXY = covXY/n;    % covariance of X and Y
-% SVD of covXY
-[U,D,V] = svd(covXY);
-% calculate @R, @s and @t
-r = rank(covXY);
-S = eye(3);
-if det(covXY)<0
-    S(3,3) = -1;
+if strcmp(mod,'vio')
+    a = covXY(2,1) - covXY(1,2);
+    b = covXY(1,1) + covXY(2,2);
+    c = sqrt(a*a+b*b);
+    sin_ = a/c;
+    cos_ = b/c;
+    R = [cos_,-sin_,0; sin_,cos_,0; 0,0,1];
+    s = 1;
+else
+    covXY = covXY/n;    % covariance of X and Y
+    % SVD of covXY
+    [U,D,V] = svd(covXY);
+    % calculate @R, @s and @t
+    r = rank(covXY);
+    S = eye(3);
+    if det(covXY)<0
+        S(3,3) = -1;
+    end
+    R = U*S*V';
+    if strcmp(mod,'mono')
+        s = trace(D*S)/sigma2_X;
+    else
+        s = 1;
+    end
+    if r<2
+        fprintf('Solution is not unique\n');
+    end
 end
-R = U*S*V';
-s = trace(D*S)/sigma2_X;
 t = mu_Y-s*R*mu_X;
-if r<2
-    fprintf('Solution is not unique\n');
-end
 end
